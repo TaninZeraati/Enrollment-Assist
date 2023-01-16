57a5a8cc9ad9bec6a296da0996bbe5023c5e4e60
TaninZeraati <80265241+TaninZeraati@users.noreply.github.com>
2023-01-17 00:03:26 +0330

    adding canBeTakenBy

--- src/test/java/bdd/canBeTakenByBDD.java
+++ src/test/java/bdd/canBeTakenByBDD.java
@@ -1,45 +1,49 @@
 package bdd;
 
-import io.cucumber.java.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import ir.proprog.enrollassist.Exception.ExceptionList;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.owner.*;
+
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-package ir.proprog.enrollassist.domain.EnrollmentRules.EnrollmentRuleViolation;
-package ir.proprog.enrollassist.domain.EnrollmentRules;
-package ir.proprog.enrollassist.domain.student;
+import ir.proprog.enrollassist.domain.EnrollmentRules.EnrollmentRuleViolation;
+import ir.proprog.enrollassist.domain.EnrollmentRules.*;
+import ir.proprog.enrollassist.domain.student.Student;
+import ir.proprog.enrollassist.domain.course.Course;
 
 public class canBeTakenByBDD {
     @Autowired
-    Cource newCource;
+    Course newCourse;
 
     @Autowired
-    Student student;
+    Student tanin;
 
 
 
 
     @Given("A Studen.")
-    public void wantsToTakeCourse(){
+    public void wantsToTakeCourse() throws ExceptionList {
         tanin = new Student("810197977", "Undergraduate");
 
     }
 
     @When("Wants to take a cource")
-    public void takeNewCource(){
-        newCource = new Course("4444444", "MATH1", 3, "Undergraduate");
+    public void takeNewCource() throws ExceptionList {
+        newCourse = new Course("4444444", "MATH1", 3, "Undergraduate");
     }
 
     @Then("Check the PreRequisits are meet")
     public void checkCanBeTaken(){
-        List<EnrollmentRuleViolation> violations = newCource.canBeTakenBy(tanin);
-        assertTrue(violations.isEmpty());
+        List<EnrollmentRuleViolation> violations = newCourse.canBeTakenBy(tanin);
+        Assertions.assertTrue(violations.isEmpty());
 
     }
 }
---
+++ target/test-classes/canBeTakenByBDD.feature
@@ -0,0 +1,6 @@
+Feature: Check Can The Cource Be Taken By
+
+    Senario: A student has passed all the prerequisits.
+        Given A Students.
+        When  Wants to take a course.
+        Then  It should be checked if he had passed the prereauisits.
